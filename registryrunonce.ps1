49d44e1696a9d9bf745a4609559f6632b3147e0c
vijai <vijai.djearam@gmail.com>
2025-02-12 08:33:28 +0100

    Update registryrunonce.ps1

--- registryrunonce.ps1
+++ registryrunonce.ps1
@@ -3,7 +3,7 @@ while (!(test-connection 8.8.8.8 -Count 1 -Quiet -ErrorAction SilentlyContinue))
     sleep 5
 }
 write-host "internet connection is up! -> proceeding with the script" -ForegroundColor Green
-$repopath ="https://raw.githubusercontent.com/vijaidjearam/startupscript/master/"
+$repopath ="https://raw.githubusercontent.com/vijaidjearam/win11install/main/"
 if (test-path 'HKCU:\repopath'){
 $repopath = Get-ItemPropertyValue -Path 'HKCU:\repopath' -Name path
 }
