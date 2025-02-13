b434bb95513ebb964a71c9cd9a4782138b5e216b
vijai <vijai.djearam@gmail.com>
2025-02-13 14:48:10 +0100

    Update windowsupdate_initiate.ps1

--- windowsupdate_initiate.ps1
+++ windowsupdate_initiate.ps1
@@ -4,12 +4,12 @@ Install-PackageProvider -Name NuGet -MinimumVersion 2.8.5.201 -Force
 Install-Module -Name PSWindowsUpdate -Force
 Import-Module -Name PSWindowsUpdate
 Set-ItemProperty -Path 'HKCU:\osinstall_local' -Name stage -value 'windowsupdate_followup'
-Set-Runonce
 #KB5000802 fails and blocks the windows update process so ignoring this update
 #Install-WindowsUpdate -NotCategory "Drivers" -NotTitle OneDrive -NotKBArticleID KB5000802 -AcceptAll -AutoReboot
 Install-WindowsUpdate -NotCategory "Drivers" -NotTitle OneDrive -AcceptAll -AutoReboot
 #Install-WindowsUpdate -AcceptAll -AutoReboot
 write-host "Stage: windowsupdate_initiate completed" -ForegroundColor Green
+Set-Runonce
 Stop-Transcript
 Restart-Computer
 }
