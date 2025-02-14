986b05c329aab04a2ec2e4fb2d13c1cce57fb785
vijai <vijai.djearam@gmail.com>
2025-02-14 09:20:17 +0100

    Update header.ps1

--- header.ps1
+++ header.ps1
@@ -152,7 +152,7 @@ Switch ($stage)
         #Start-Transcript -path $FileName -NoClobber
         ((New-Object System.Net.WebClient).DownloadString($repopath+"win10_debloat.ps1")) | Out-File $env:TEMP\win10_debloat.ps1
         ((New-Object System.Net.WebClient).DownloadString($repopath+"win10_debloat.psm1"))| Out-File $env:TEMP\win10_debloat.psm1
-        powershell.exe -NoProfile -ExecutionPolicy Bypass -File $env:TEMP\win10_debloat.ps1 -include $env:TEMP\win10_debloat.psm1 -preset C:\Windows\Setup\Scripts\debloat_preset.txt -log $LogFileName
+        powershell.exe -NoProfile -ExecutionPolicy Bypass -File $env:TEMP\win10_debloat.ps1 -include $env:TEMP\win10_debloat.psm1 -include $env:TEMP\windows_settings.psm1 -preset C:\Windows\Setup\Scripts\debloat_preset.txt -log $LogFileName
         #((New-Object System.Net.WebClient).DownloadString($repopath+"debloat_preset.txt"))| Out-File $env:TEMP\debloat_preset.txt
         # powershell.exe -NoProfile -ExecutionPolicy Bypass -File $env:TEMP\win10_debloat.ps1 -include $env:TEMP\win10_debloat.psm1 -include $env:TEMP\windows_settings.psm1 -preset $env:TEMP\debloat_preset.txt
         write-host "Stage: windows_debloat completed" -ForegroundColor Green
