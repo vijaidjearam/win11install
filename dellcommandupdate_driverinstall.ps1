a1268576b96630713c3f449f581a5e8b16ef1f85
vijai <vijai.djearam@gmail.com>
2025-02-25 12:34:05 +0100

    Update dellcommandupdate_driverinstall.ps1

--- dellcommandupdate_driverinstall.ps1
+++ dellcommandupdate_driverinstall.ps1
@@ -13,7 +13,7 @@ $dcuCLI = "C:\Program Files (x86)\Dell\CommandUpdate\dcu-cli.exe"
 # Configure advanced driver restore and show output
 try {
     Start-Process -FilePath "cmd.exe" -ArgumentList "/c `"$dcuCLI /configure -advancedDriverRestore=enable`"" -Wait
-    Write-Host "Dell Command Update configuration completed." -ForegroundColor Green
+    Write-Host "Dell Command Update Enable advancedDriverRestore ." -ForegroundColor Green
 } catch {
     Write-Host "Failed to configure Dell Command Update: $_" -ForegroundColor Red
     throw
