79cae2155be9e0b42c1a4fa2b6eb21d7b74a10f4
vijai <vijai.djearam@gmail.com>
2025-02-26 09:14:08 +0100

    Update dellcommandupdate_driverinstall.ps1

--- dellcommandupdate_driverinstall.ps1
+++ dellcommandupdate_driverinstall.ps1
@@ -8,13 +8,14 @@ try {
 }
 # Configure advanced driver restore
 & "C:\Program Files (x86)\Dell\CommandUpdate\dcu-cli.exe" /configure -advancedDriverRestore=enable
+write-host "`n Advanced Driver Restore Enabled" -ForegroundColor Green
 # Run driver installation
 & "C:\Program Files (x86)\Dell\CommandUpdate\dcu-cli.exe" /driverInstall
 #if ($? -and ($LASTEXITCODE -in @(0, 1, 5, 500)))
 # Checking if the driver install went through successfully
 if ($LASTEXITCODE -in @(0, 1, 5, 500))
 {
-    write-host "Stage: dellcommandupdate_driverinstall completed" -ForegroundColor Green
+    write-host "`nStage: dellcommandupdate_driverinstall completed" -ForegroundColor Green
     Set-ItemProperty -Path 'HKCU:\osinstall_local' -Name stage -value 'dellcommandupdate_applyupdates'
     Set-Runonce
     Stop-Transcript
@@ -22,7 +23,7 @@ if ($LASTEXITCODE -in @(0, 1, 5, 500))
 } 
 else 
 {
-    write-host "Stage: dellcommandupdate_driverinstall Failed" -ForegroundColor Red
+    write-host "`nStage: dellcommandupdate_driverinstall Failed" -ForegroundColor Red
     Set-ItemProperty -Path 'HKCU:\osinstall_local' -Name stage -value dellcommandupdate_driverinstall
     Set-Runonce
     Stop-Transcript
