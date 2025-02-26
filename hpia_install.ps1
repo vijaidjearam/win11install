79060d99ce1fd7a9f9b663df160d1d7518e15afb
vijai <vijai.djearam@gmail.com>
2025-02-26 10:56:16 +0100

    Update hpia_install.ps1

--- hpia_install.ps1
+++ hpia_install.ps1
@@ -10,11 +10,11 @@ while (-not $resolved) {
         }
     } catch {
         Write-Host "Failed to resolve $site. Retrying..." -ForegroundColor Red
-        Start-Sleep -Seconds 5  # Wait for 5 seconds before trying again
+        Start-Sleep -Seconds 10  # Wait for 10 seconds before trying again
     }
 }
 choco install dotnet hpia -y
-if($LASTEXITCODE -eq 0 -or $LASTEXITCODE -eq 1 -or $LASTEXITCODE -eq 5 -or $LASTEXITCODE -eq 500)
+if ($LASTEXITCODE -in @(0, 1, 5, 500))
 {
 write-host "Stage: HP Image Assist installation completed" -ForegroundColor Green
 Set-ItemProperty -Path 'HKCU:\osinstall_local' -Name stage -value 'Hpia_driverinstall'
