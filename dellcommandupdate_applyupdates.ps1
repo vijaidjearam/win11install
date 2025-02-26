aa86451c04af4cd246ea3dedca7c18b9bbf13b7a
vijai <vijai.djearam@gmail.com>
2025-02-26 09:17:03 +0100

    Update dellcommandupdate_applyupdates.ps1

--- dellcommandupdate_applyupdates.ps1
+++ dellcommandupdate_applyupdates.ps1
@@ -11,7 +11,7 @@ Restart-Computer
 else 
 {
 write-host "Stage: dellcommandupdate_applyupdates Failed" -ForegroundColor Red
-Set-ItemProperty -Path 'HKCU:\osinstall_local' -Name stage -value dellcommandupdate_applyupdates
+Set-ItemProperty -Path 'HKCU:\osinstall_local' -Name stage -value 'dellcommandupdate_applyupdates'
 Set-Runonce
 Stop-Transcript
 Pause
