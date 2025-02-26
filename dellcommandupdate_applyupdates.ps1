0fcb236440579c05e57e1cf0f883dd7b6cfd5dfe
vijai <vijai.djearam@gmail.com>
2025-02-26 09:19:38 +0100

    Update dellcommandupdate_applyupdates.ps1

--- dellcommandupdate_applyupdates.ps1
+++ dellcommandupdate_applyupdates.ps1
@@ -2,17 +2,17 @@
 
 if ($LASTEXITCODE -in @(0, 1, 5, 500))
 {
-write-host "Stage: dellcommandupdate_applyupdates completed" -ForegroundColor Green
-Set-ItemProperty -Path 'HKCU:\osinstall_local' -Name stage -value 'dellcommandconfigure'
-Set-Runonce
-Stop-Transcript
-Restart-Computer
+  write-host "Stage: dellcommandupdate_applyupdates completed" -ForegroundColor Green
+  Set-ItemProperty -Path 'HKCU:\osinstall_local' -Name stage -value 'dellcommandconfigure'
+  Set-Runonce
+  Stop-Transcript
+  Restart-Computer
 } 
 else 
 {
-write-host "Stage: dellcommandupdate_applyupdates Failed" -ForegroundColor Red
-Set-ItemProperty -Path 'HKCU:\osinstall_local' -Name stage -value 'dellcommandupdate_applyupdates'
-Set-Runonce
-Stop-Transcript
-Pause
+  write-host "Stage: dellcommandupdate_applyupdates Failed" -ForegroundColor Red
+  Set-ItemProperty -Path 'HKCU:\osinstall_local' -Name stage -value 'dellcommandupdate_applyupdates'
+  Set-Runonce
+  Stop-Transcript
+  Pause
 }
