52bcb3d5eba3d0620a07b946d902b4d56e30b6f7
vijai <vijai.djearam@gmail.com>
2025-05-14 14:49:28 +0200

    cleaning stage updated

--- header.ps1
+++ header.ps1
@@ -151,7 +151,7 @@ Switch ($stage)
         #Start-Transcript -path $FileName -NoClobber
         #iex ((New-Object System.Net.WebClient).DownloadString($repopath+'cleaning.ps1'))
         $LogFileName = $env:TEMP+"\"+(Get-Date).tostring("dd-MM-yyyy-hh-mm-ss")+"_"+ $stage+"_log.log"
-        ((New-Object System.Net.WebClient).DownloadString($repopath+'windows_settings.ps1'))| Out-File $env:TEMP\cleaning.ps1
+        ((New-Object System.Net.WebClient).DownloadString($repopath+'cleaning.ps1'))| Out-File $env:TEMP\cleaning.ps1
         powershell.exe -NoProfile -ExecutionPolicy Bypass -File $env:TEMP\cleaning.ps1 -include $env:TEMP\windows_settings.psm1 -preset C:\Windows\Setup\Scripts\cleaning_settings.txt -log $LogFileName
     }
 }
