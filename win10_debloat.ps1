ed16670a1e9a9ad338d2c796b3beab08a07a6f30
vijai <vijai.djearam@gmail.com>
2025-05-14 14:28:33 +0200

    Update win10_debloat.ps1

--- win10_debloat.ps1
+++ win10_debloat.ps1
@@ -67,7 +67,7 @@ write-host $_ "---------------------OK" -ForegroundColor Green
 catch{
 write-host  ""
 write-host $_ "--------------Nok" -ForegroundColor Red
-write-host "Stage: Cleaning Failed" -ForegroundColor Red
+write-host "Stage: Windows-Debloat Failed" -ForegroundColor Red
 Set-ItemProperty -Path 'HKCU:\osinstall_local' -Name stage -value 'windows_debloat'
 Set-Runonce
 #Stop-Transcript
@@ -75,7 +75,7 @@ Pause
 Exit
 }
 }
-write-host "Stage: Cleaning completed" -ForegroundColor Green
+write-host "Stage: Windows-Debloat completed" -ForegroundColor Green
 Set-ItemProperty -Path 'HKCU:\osinstall_local' -Name stage -value 'cleaning'
 Set-Runonce
 #Stop-Transcript
