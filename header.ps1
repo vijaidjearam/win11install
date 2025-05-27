870a0ef6a999a6103c5e55ffac132183707e0969
vijai <vijai.djearam@gmail.com>
2025-05-27 11:27:56 +0200

    Update header.ps1

--- header.ps1
+++ header.ps1
@@ -32,7 +32,7 @@ New-ItemProperty -Path 'HKCU:\osinstall_local' -Name stage -value 'dellcommandup
 elseif ($manufacturer -like '*hp*')
 {
 write-host "System manufacturer has been detected as HP - so proceeding with Dell driver update" -ForegroundColor Green
-New-ItemProperty -Path 'HKCU:\osinstall_local' -Name stage -value 'hpia_install'
+New-ItemProperty -Path 'HKCU:\osinstall_local' -Name stage -value 'chocolatey_apps'
 }
 else
 {
